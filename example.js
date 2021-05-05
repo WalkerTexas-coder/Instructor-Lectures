b056e97b85232a23808950fa7db65455ff3b7716
WalkerTexas-coder <austinsrwalker@gmail.com>
2021-05-04 17:04:25 -0700

    changes

---
+++ example.js
@@ -0,0 +1,30 @@
+console.log(34 + 71);
+console.log(123 - 67);
+console.log(56 * 23);
+console.log(5^7);
+console.log(33 % 6);
+console.log("Austin".length);
+console.log('Austin'[0]);
+var greeting = "hello"
+console.log(greeting.includes("e"));
+console.log(greeting.toUpperCase());
+console.log((34 / 3) > (67 / 2));
+console.log(5 == "5");
+console.log( 5 === "5");
+console.log(!3 === 3);
+console.log(("LEARN".lenght === 5) && ("STUDENT".length === 7));
+console.log(("LEARN".lenght === 5) || ("STUDENT".length === 7));
+console.log("LEARN".includes("RN"));
+console.log("LEARN"[0].includes("rn"));
+console.log("LEARN"[0] === "l");
+console.log("LEARN"[0] === "L");
+var theQuestion = "life, the universe, and everything"
+var theAnswer = 42
+console.log(theAnswer / 2);
+console.log(theQuestion.length);
+console.log(theQuestion.indexOf("f"));
+console.log(theQuestion + theAnswer);
+console.log(theQuestion.split(" ")[1]+ " " +theQuestion.split(" ")[2]);
+// var indexOfI = theQuestion.indexOf('i')
+console.log(theQuestion[theQuestion.indexOf('i')])
+console.log(theQuestion.length > theAnswer.length);
